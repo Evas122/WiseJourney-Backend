63cf53430ea5dc3d592477e3664571b9b3190c68
Evas122 <108418453+Evas122@users.noreply.github.com>
2024-12-06 15:49:34 +0100

    Optimization/validators (#5)
    * added validators to commands and one to query
    
    * fix place types in prompt occasionally
    
    * forget to save

---
+++ src/WiseJourneyBackend.Application/Commands/CreateTrip/CreateTripValidator.cs
@@ -0,0 +1,48 @@
+﻿using FluentValidation;
+
+namespace WiseJourneyBackend.Application.Commands.CreateTrip;
+
+public class CreateTripValidator : AbstractValidator<CreateTripCommand>
+{
+    public CreateTripValidator()
+    {
+        RuleFor(x => x.Name)
+            .NotEmpty().WithMessage("Trip name is required.");
+
+        RuleFor(x => x.StartDateUtc)
+            .LessThan(x => x.EndDateUtc).WithMessage("Start date must be before end date.");
+
+        RuleFor(x => x.TripDays)
+            .NotEmpty().WithMessage("Trip must have at least one day.")
+            .Must(days => days.Select(d => d.DateUtc).Distinct().Count() == days.Count)
+            .WithMessage("Trip days must not have duplicate dates.");
+
+        RuleForEach(x => x.TripDays)
+            .SetValidator(new CreateTripDayValidator());
+    }
+
+    public class CreateTripDayValidator : AbstractValidator<CreateTripDay>
+    {
+        public CreateTripDayValidator()
+        {
+            RuleFor(x => x.DateUtc)
+                .NotEmpty().WithMessage("Day date is required.");
+
+            RuleForEach(x => x.TripPlaces)
+                .SetValidator(new CreateTripPlaceValidator());
+        }
+    }
+
+    public class CreateTripPlaceValidator : AbstractValidator<CreateTripPlace>
+    {
+        public CreateTripPlaceValidator()
+        {
+            RuleFor(x => x.PlaceId)
+                .NotEmpty().WithMessage("Place ID is required.");
+
+            RuleFor(x => x.ScheduleTimeUtc)
+                .GreaterThan(DateTime.MinValue).When(x => x.ScheduleTimeUtc.HasValue)
+                .WithMessage("Schedule time must be a valid date.");
+        }
+    }
+}
---
+++ src/WiseJourneyBackend.Application/Queries/GetPagedTrips/GetPagedTripsValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+
+namespace WiseJourneyBackend.Application.Queries.GetPagedTrips;
+
+public class GetPagedTripsValidator : AbstractValidator<GetPagedTripsQuery>
+{
+    public GetPagedTripsValidator()
+    {
+        RuleFor(x => x.Page)
+            .GreaterThan(0)
+            .WithMessage("Page number cannot be lower than 0.");
+
+        RuleFor(x => x.PageSize)
+            .GreaterThan(0)
+            .LessThanOrEqualTo(10)
+            .WithMessage("Page size cannot be lower than 0");
+
+    }
+}
